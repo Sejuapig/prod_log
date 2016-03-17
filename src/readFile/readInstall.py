b663a1c79f9932d34d26baa99ef2ec7ce33f7ec9
MedinaAlex <alex.medina@hotmail.fr>
2016-03-17 09:07:45 +0100

    mis aj our base de donnée pour sqlite3

--- src/readFile/readInstall.py
+++ src/readFile/readInstall.py
@@ -1,18 +1,17 @@
 import csv
 
-def lunch():
-    return read("/hometu/etudiants/m/e/E145465P/info2/prod_log/Production_logiciel/Installations.csv")
-     
+def run():
+	return read("/hometu/etudiants/m/e/E145465P/info2/prod_log/Production_logiciel/csvFiles/Installations.csv")
+	 
 def read(path):
-    fichier = csv.reader(open(path,"rt"), delimiter=',')
-    
-    tableau= []
-        
-    for row in fichier:
-       tableau.append([row[1],row[0],row[7],row[4],row[2],row[9],row[10]]) #numero, nom et coordonées
-    
-    
-    return tableau[1:]
+	fichier = csv.reader(open(path,"rt"), delimiter=',')
+	
+	tableau= []
+		
+	for row in fichier:
+	   tableau.append([row[1],row[0],row[7],row[4],row[2],row[9],row[10]]) #numero, nom et coordonées
+
+	return tableau[1:]
+
 if __name__ == '__main__':
-    read("/hometu/etudiants/m/e/E145465P/info2/prod_log/Production_logiciel/Installations.csv")
-    
+	read("/hometu/etudiants/m/e/E145465P/info2/prod_log/Production_logiciel/csvFiles/Installations.csv")
