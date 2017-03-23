d6c120cc166472644e4abe4c09247e65f4541ace
Mathis ABDELAZIZ <sejuapig@gmail.com>
2017-03-23 10:09:25 +0100

    vidange

--- src/runServer.py
+++
@@ -1,9 +0,0 @@
-import RestServer.rest_PDL as pdl
-
-def run():
-	"""Function allowing you to launch the server with the import"""
-	print('test Server')
-	#pdl.run()
-
-if __name__ == '__main__':
-	run()
