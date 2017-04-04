69f1a2aecf1488a5a5ca9c259b8234813bcd625c
Nicolas CHOUIN <nicolas.chouin@etu.univ-nantes.fr>
2017-04-04 09:33:58 +0200

    indentation

--- src/admin/connexion.py
+++ src/admin/connexion.py
@@ -1,6 +1,6 @@
 import mysql.connector as connector
 def connexion():
-"""Function allowing you to connect to the database and return the object connector"""
+	"""Function allowing you to connect to the database and return the object connector"""
 	try:
 		return connector.connect(host = "infoweb", user = "E145725X", password ="E145725X", database = "E145725X")
 	except MySQLdb.Error as e :
